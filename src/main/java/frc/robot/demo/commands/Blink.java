4e1bb8b555483f69f5557d62e91b41791585699b
unknown <team2393@laptop.org>
2020-01-03 11:20:40 -0500

    Command example

--- src/main/java/frc/robot/demo/commands/Blink.java
+++ src/main/java/frc/robot/demo/commands/Blink.java
@@ -17,22 +17,36 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class Blink extends CommandBase
 {
   private final DigitalOutput output;
-  private final long ms;
+  private final long period_ms;
+  private long start_ms = 0;
 
   /** @param output Output to use
-   *  @param ms On/off period in milliseconds
+   *  @param period_ms On/off period in milliseconds
    */
-  public Blink(final DigitalOutput output, final long ms)
+  public Blink(final DigitalOutput output, final long period_ms)
   {
     this.output = output;
-    this.ms = ms;
+    this.period_ms = period_ms;
+  }
+
+  @Override
+  public void initialize()
+  {
+    // Remember when command started so that
+    // we always blink with a full 'on' period
+    // when started, instead of initially being on/off
+    // depending on the currentTimeMillis
+    start_ms = System.currentTimeMillis();
   }
 
   @Override
   public void execute()
   {
-    // Determine if output should be on or off
-    final boolean on_off = (System.currentTimeMillis() / ms) % 2 == 1;
+    // Determine if output should be on or off:
+    // Check how much time has passed since start_ms,
+    // convert into periods,
+    // turn 'on' for all even periods, including period 0.
+    final boolean on_off = ((System.currentTimeMillis() - start_ms) / period_ms) % 2 == 0;
     output.set(on_off);
   }
 
--- src/main/java/frc/robot/demo/commands/CommandRobot.java
+++ src/main/java/frc/robot/demo/commands/CommandRobot.java
@@ -8,7 +8,12 @@
 package frc.robot.demo.commands;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.BasicRobot;
 
 /** Demo of 'new' commands
@@ -17,8 +22,52 @@ import frc.robot.BasicRobot;
  */
 public class CommandRobot extends BasicRobot
 {
+    // In larger robot project, the port numbers should be in a RobotMap..
     private final DigitalOutput led = new DigitalOutput(1);
+
+    // .. and the operator interface details should be in an OI class...
+    private final XboxController joystick = new XboxController(0);
+    private final JoystickButton test1 = new JoystickButton(joystick, XboxController.Button.kA.value);
+    private final JoystickButton test2 = new JoystickButton(joystick, XboxController.Button.kB.value);
+
+    // Our command that blinks
     private final Command blink = new Blink(led, 500);
+
+    // Same functionality, but assembled from basic commands
+    private final Command led_on = new InstantCommand(() -> led.set(true));
+    private final Command led_off = new InstantCommand(() -> led.set(false));
+    private final Command blink_seq = new SequentialCommandGroup(led_on,
+                                                                 new WaitCommand(0.5),
+                                                                 led_off,
+                                                                 new WaitCommand(0.5))
+    {
+        @Override
+        public void end(boolean interrupted)
+        {
+            // Always make sure that LED is off
+            led.set(false);
+            // .. then do whatever SequentialCommandGroup does to end()
+            super.end(interrupted);
+        }
+    };
+
+    @Override
+    public void robotInit()
+    {
+        super.robotInit();
+        System.out.print("Hold button A or B to blink on DIO 1");
+
+        // Bind Joystick to commands (used in teleop)
+        // Note subtle difference:
+        // Pressing the button starts our 'Blink' command,
+        // which then runs forever until releasing the button ends it.
+        // On end(), the LED will be turned off.
+        test1.whileHeld(blink);
+        // Pressing the button starts the blink_seq, which blinks once,
+        // then ends, and keeps getting restarted while the button is held.
+        // When the button is released, end() will turn the LED off.
+        test2.whileHeld(blink_seq);
+    }
 
     @Override
     public void autonomousInit()
