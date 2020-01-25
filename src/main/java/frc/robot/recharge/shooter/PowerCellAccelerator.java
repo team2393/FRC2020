5371296187237155f434fccd0208aac5d7740be1
Kay <kay.kasemir@gmail.com>
2020-01-25 08:38:51 -0500

    Clarify

--- src/main/java/frc/robot/recharge/shooter/PowerCellAccelerator.java
+++ src/main/java/frc/robot/recharge/shooter/PowerCellAccelerator.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2020 FIRST Team 2393. All Rights Reserved.                   */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -14,10 +14,17 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.recharge.RobotMap;
 
-
+/** Power cell handling
+ * 
+ *  Intake picks 'fuel cell' balls from floor into hopper.
+ *  From hopper they drop onto horizontal conveyor belt.
+ *  Horizontal belt moves them to vertical conveyor,
+ *  which feeds them to ejector/shooter.
+ *  Angle of rotatable hood/shield/deflector adjusts
+ *  the angle at which balls are ejected.
+ */
 public class PowerCellAccelerator extends SubsystemBase 
 {
-
   // Motors
   // TODO figure out what type of motor controllers will actually be used
   private final WPI_TalonFX shooting_motor = new WPI_TalonFX(RobotMap.SHOOTER_MOTOR);
@@ -25,7 +32,7 @@ public class PowerCellAccelerator extends SubsystemBase
   private final WPI_TalonSRX intake_motor = new WPI_TalonSRX(RobotMap.INTAKE_MOTOR);
   private final WPI_TalonSRX angle_adjustment = new WPI_TalonSRX(RobotMap.ANGLE_MOTOR);
   
-  // Sensors
+  // Sensors: Banner T8??6D50
   private final DigitalInput ball_at_end_of_conveyor = new DigitalInput(RobotMap.BALL_AT_END_OF_CONVEYOR);
   private final DigitalInput ball_at_end_of_ejector = new DigitalInput(RobotMap.BALL_AT_END_OF_EJECTOR);
   
@@ -56,15 +63,24 @@ public class PowerCellAccelerator extends SubsystemBase
     return shooting_motor.getSelectedSensorVelocity();
   }
 
+  /** @return true if there's a ball at the end of the (vertical) conveyer belt,
+   *          just before the shooter.
+   *          If converyor keeps moving, that ball will be ejected
+   */
   public boolean isStorageFull()
   {
     // Sensor is inverted, 'false' == seeing a ball
-    return !ball_at_end_of_conveyor.get(); // TODO Maybe turn this into a int that returns amount of balls in storage depending on sensor layout
+    return !ball_at_end_of_conveyor.get();
   }
-
+  
+  /** @return true if a ball is detected at the ejector
+   *          (will only be briefly true since ball is then ejected)
+   */
   public boolean isBallEjected()
   {
     // Sensor is inverted, 'false' == seeing a ball
     return !ball_at_end_of_ejector.get();
   }
+
+  // TODO Maybe have a way to report the amount of balls in storage depending on sensor layout
 }
