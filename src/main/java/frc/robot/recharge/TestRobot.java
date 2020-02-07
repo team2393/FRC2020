9f2c49f02d20b364248dc5c3c2e4bf4e5cdbbdab
Kay <kay.kasemir@gmail.com>
2020-02-07 07:19:17 -0500

    Beep

--- src/main/java/frc/robot/recharge/TestRobot.java
+++ src/main/java/frc/robot/recharge/TestRobot.java
@@ -9,15 +9,12 @@ package frc.robot.recharge;
 
 import java.util.List;
 
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.music.Orchestra;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.BasicRobot;
-import frc.robot.recharge.led.LEDStrip;
+import frc.robot.recharge.sound.BeepBipBipBeeeep;
 
 /** Robot code for testing devices */
 public class TestRobot extends BasicRobot
@@ -25,51 +22,15 @@ public class TestRobot extends BasicRobot
   // private final DigitalInput ball = new DigitalInput(8);
   // private final LEDStrip led = new LEDStrip();
 
-  private final TalonFX talon = new TalonFX(1);
-  private final Orchestra orch = new Orchestra(List.of(talon));
-
-  @Override
-  public void disabledPeriodic()
-  {
-  }
-
-  final int[] notes =
-  { 
-    300,
-      0,
-      0,
-    300,
-      0,
-    300,
-      0,
-    600,
-    600,
-    600,
-      0,
-      0,
-      0,
-      0
-  };
-
-  private Timer timer = new Timer();
+  private final List<TalonFX> instruments = List.of(new TalonFX(1));
+  private final Orchestra orch = new Orchestra(instruments);
+  private final CommandBase beep = new BeepBipBipBeeeep(instruments);
 
   @Override
   public void teleopInit()
   {
     super.teleopInit();
-    timer.start();
-  }
-
-  @Override
-  public void teleopPeriodic()
-  {
-    int note = (int) (timer.get() / 0.1);
-    if (note >= notes.length)
-    {
-      note = 0;
-      timer.reset();
-    }
-    talon.set(TalonFXControlMode.MusicTone, notes[note]);
+    beep.schedule();
   }
 
   @Override
@@ -78,10 +39,5 @@ public class TestRobot extends BasicRobot
     super.autonomousInit();
     System.out.println(orch.loadMusic("tune.chrp"));
     System.out.println(orch.play());
-  }
-
-  @Override
-  public void autonomousPeriodic()
-  {
   }
 }
