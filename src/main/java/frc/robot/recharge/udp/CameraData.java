7b3eb7e9943fc38e183b602b273342512fb70233
Kay <kay.kasemir@gmail.com>
2020-03-08 17:50:32 -0500

    Guesstimator...

--- src/main/java/frc/robot/recharge/udp/CameraData.java
+++ src/main/java/frc/robot/recharge/udp/CameraData.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2020 FIRST Team 2393. All Rights Reserved.                   */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,16 +7,26 @@
 
 package frc.robot.recharge.udp;
 
-/**
- * Add your docs here.
- */
+/** Information received from camera */
 public class CameraData 
 {
+  /** Time in milliseconds when data was received */
+  public long millisec;
+
+  /** Horizontal direction to target, 'right' is positive */
   public int direction;
+
+  /** Vertical indicator of distance to target, 'up' is positive */
   public int distance;
-  
-  public CameraData(int direction, int distance)
+
+  public CameraData(final int direction, final int distance)
   {
+    this(System.currentTimeMillis(), direction, distance);
+  }
+
+  public CameraData(final long millisec, final int direction, final int distance)
+  {
+    this.millisec = millisec;
     this.direction = direction;
     this.distance = distance;
   }
